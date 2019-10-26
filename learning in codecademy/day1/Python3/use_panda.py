16afc4bd9e75a721d497e01fb88a11b858c1566c
Cahn <670686205@qq.com>
2019-10-26 10:50:24 +0800

    study1

---
+++ learning in codecademy/day1/Python3/use_panda.py
@@ -0,0 +1,10 @@
+import codecademylib3_seaborn
+import pandas as pd
+# Paste code here:
+
+
+# Load data
+df = pd.read_csv('page_visits.csv')
+
+# Display data
+print(df.head())
---
+++ learning in codecademy/day1/SQL/select.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM browse
+LIMIT 10;
+/*This code will select all (*) columns from browse table for the first 10 records*/
